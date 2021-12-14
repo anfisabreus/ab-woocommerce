f5edff255eacfef3cc1f0ceecc494d5758b86b04
Anfisa Breus <anfisabreus@gmail.com>
2021-12-14 12:15:24 +0300

    2.18-14.12.2021
    Перенос функции для отображения похожих товаров по 4 в ряд в шаблон AB-inspiration

--- woocommerce-functions.php
+++ woocommerce-functions.php
@@ -586,20 +586,3 @@ $new_rate['cost']  += 2;
 // Add it to WC			
 $method->add_rate( $new_rate );		}
 */
-
-
-/**
- * Change number of related products output
- */ 
-function woo_related_products_limit() {
-  global $product;
-	
-	$args['posts_per_page'] = 6;
-	return $args;
-}
-add_filter( 'woocommerce_output_related_products_args', 'jk_related_products_args', 20 );
-  function jk_related_products_args( $args ) {
-	$args['posts_per_page'] = 4; // 4 related products
-	$args['columns'] = 4; // arranged in 2 columns
-	return $args;
-}
